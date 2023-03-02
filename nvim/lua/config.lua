0870f7fdaf19b3cdea7f343a4ad30621300c0ef6
Alexander Flink <hi@alexanderflink.com>
2023-03-02 21:33:30 +0100

    initial commit

---
+++ nvim/lua/config.lua
@@ -0,0 +1,14 @@
+vim.wo.relativenumber = true
+-- highlight on yank
+vim.api.nvim_command("au TextYankPost * silent! lua vim.highlight.on_yank()")
+vim.o.guifont = "Hack Nerd Font Mono:h13"
+-- enter terminal mode when opening a terminal
+-- vim.api.nvim_create_autocmd({ "BufEnter", "TermOpen" }, { pattern = { "term://*" }, command = "startinsert" })
+-- always show tabline
+vim.o.showtabline = 2
+vim.cmd("colorscheme duskfox")
+-- auto format on save
+vim.api.nvim_create_autocmd({ "BufWritePost" }, { command = "FormatWrite" })
+vim.opt.cursorline = true
+vim.opt.signcolumn = "yes"
+vim.opt.clipboard = "unnamedplus"
---
+++ nvim/lua/packer-plugins.lua
@@ -0,0 +1,90 @@
+return require("packer").startup(function(use)
+	use("wbthomason/packer.nvim")
+	use("karb94/neoscroll.nvim")
+	use({ "nvim-treesitter/nvim-treesitter", run = ":TSUpdate" })
+	use("ishan9299/nvim-solarized-lua")
+	use("nvim-lua/plenary.nvim")
+	use("mhartington/formatter.nvim")
+	use({
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.0",
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
+	use("nvim-telescope/telescope-file-browser.nvim")
+	use({ "nvim-tree/nvim-tree.lua", commit = "8b8d457" })
+	use("nvim-lualine/lualine.nvim")
+	use("tpope/vim-surround")
+	use("wellle/targets.vim")
+	use("windwp/nvim-autopairs")
+	use("tpope/vim-fugitive")
+	use("windwp/nvim-ts-autotag")
+	use("tpope/vim-commentary")
+	use("lewis6991/gitsigns.nvim")
+	use("folke/which-key.nvim")
+	use("nvim-tree/nvim-web-devicons")
+	use("tpope/vim-sleuth")
+	use("JoosepAlviste/nvim-ts-context-commentstring")
+	use("nanozuki/tabby.nvim")
+	use("EdenEast/nightfox.nvim")
+	use({
+		"VonHeikemen/lsp-zero.nvim",
+		requires = {
+			-- LSP Support
+			{ "neovim/nvim-lspconfig" },
+			{ "williamboman/mason.nvim" },
+			{ "williamboman/mason-lspconfig.nvim" },
+
+			-- Autocompletion
+			{ "hrsh7th/nvim-cmp" },
+			{ "hrsh7th/cmp-buffer" },
+			{ "hrsh7th/cmp-path" },
+			{ "saadparwaiz1/cmp_luasnip" },
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{ "hrsh7th/cmp-nvim-lua" },
+
+			-- Snippets
+			{ "L3MON4D3/LuaSnip" },
+			{ "rafamadriz/friendly-snippets" },
+		},
+	})
+	use("github/copilot.vim")
+	-- use({
+	-- 	"zbirenbaum/copilot.lua",
+	-- 	event = "VimEnter",
+	-- 	config = function()
+	-- 		vim.defer_fn(function()
+	-- 			require("copilot").setup()
+	-- 		end, 100)
+	-- 	end,
+	-- })
+
+	-- use({
+	-- 	"zbirenbaum/copilot-cmp",
+	-- 	after = { "copilot.lua" },
+	-- 	config = function()
+	-- 		require("copilot_cmp").setup()
+	-- 	end,
+	-- })
+	use("tpope/vim-abolish")
+	use({
+		"folke/trouble.nvim",
+		requires = "nvim-tree/nvim-web-devicons",
+		config = function()
+			require("trouble").setup()
+		end,
+	})
+	use({
+		"folke/todo-comments.nvim",
+		requires = "nvim-lua/plenary.nvim",
+		config = function()
+			require("todo-comments").setup()
+		end,
+	})
+	use("wakatime/vim-wakatime")
+	use({
+		"akinsho/toggleterm.nvim",
+		config = function()
+			require("toggleterm").setup()
+		end,
+	})
+end)
