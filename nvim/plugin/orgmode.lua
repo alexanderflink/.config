2d1d065e8ff98b1c21640dca81c3fd25ec086bea
Alexander Flink <hi@alexanderflink.com>
2023-03-08 16:30:19 +0100

    update orgmode bindings

--- nvim/plugin/orgmode.lua
+++ nvim/plugin/orgmode.lua
@@ -9,4 +9,9 @@ require("nvim-treesitter.configs").setup({
 require("orgmode").setup({
 	org_agenda_files = { "/Users/alexanderflink/Library/Mobile Documents/com~apple~CloudDocs/Orgmode/*" },
 	org_default_notes_file = "/Users/alexanderflink/Library/Mobile Documents/com~apple~CloudDocs/Orgmode/refile.org",
+	mappings = {
+		org = {
+			org_toggle_checkbox = "<CR>",
+		},
+	},
 })
