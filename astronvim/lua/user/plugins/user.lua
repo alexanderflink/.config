6bd9cf0aa547225c07e905db8d4b4196097b6897
Alexander Flink <hi@alexanderflink.com>
2023-04-11 22:34:34 +0200

    switch to astronvim

---
+++ astronvim/lua/user/plugins/user.lua
@@ -0,0 +1,12 @@
+return {
+  -- nord theme
+  { "shaunsingh/nord.nvim" },
+  -- git integration
+  { "tpope/vim-fugitive", cmd = "G" },
+
+  -- coding stats
+  { "wakatime/vim-wakatime" },
+
+  -- todo comments
+  { "folke/todo-comments.nvim", config = true },
+}
