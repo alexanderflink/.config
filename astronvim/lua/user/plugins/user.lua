80ac2f2fbe5de52d6ded67c0492239eb3a921d2d
Alexander Flink <hi@alexanderflink.com>
2023-04-12 20:39:44 +0200

    update astronvim config

--- astronvim/lua/user/plugins/user.lua
+++ astronvim/lua/user/plugins/user.lua
@@ -2,40 +2,36 @@ return {
   -- nord theme
   { "shaunsingh/nord.nvim" },
   -- git integration
-  { "tpope/vim-fugitive", cmd = "G" },
+  { "tpope/vim-fugitive",       cmd = "G" },
 
   -- coding stats
-  { "wakatime/vim-wakatime", lazy = false },
+  { "wakatime/vim-wakatime",    lazy = false },
 
   -- todo comments
   { "folke/todo-comments.nvim", config = true },
- 
+
   -- animate scroll
   {
     "karb94/neoscroll.nvim",
     lazy = false,
     config = true,
   },
- 
+
   -- surround things
-  { "kylechui/nvim-surround", lazy = false, config = true },
+  { "kylechui/nvim-surround",   lazy = false,  config = true },
 
   -- add more text objects, like functions and arguments
   {
     "echasnovski/mini.ai",
     lazy = false,
-    config = function()
-      require("mini.ai").setup()
-    end,
+    config = function() require("mini.ai").setup() end,
   },
- 
+
   -- jump around with "s"
   {
     "ggandor/leap.nvim",
     lazy = false,
-    config = function()
-      require("leap").add_default_mappings()
-    end,
+    config = function() require("leap").add_default_mappings() end,
   },
 
   -- better "f" and "t"
@@ -44,12 +40,12 @@ return {
     config = true,
     lazy = false,
     dependencies = {
-      "tpope/vim-repeat"
-    }
+      "tpope/vim-repeat",
+    },
   },
- 
-  -- smarter substitue, among other things
-  { "tpope/vim-abolish", lazy = false },
+
+  -- smarter substitute, among other things
+  { "tpope/vim-abolish",        lazy = false },
 
   -- todo comments
   { "folke/todo-comments.nvim", config = true, lazy = false },
