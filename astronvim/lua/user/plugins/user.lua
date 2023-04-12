bfbd7b7979ac2c4008e1b23a6adead362d796767
Alexander Flink <hi@alexanderflink.com>
2023-04-12 13:16:21 +0200

    add plugins

--- astronvim/lua/user/plugins/user.lua
+++ astronvim/lua/user/plugins/user.lua
@@ -9,4 +9,45 @@ return {
 
   -- todo comments
   { "folke/todo-comments.nvim", config = true },
+ 
+  -- animate scroll
+  {
+    "karb94/neoscroll.nvim",
+    lazy = false,
+    config = true,
+  },
+ 
+  -- surround things
+  { "kylechui/nvim-surround", lazy = false, config = true },
+
+  -- add more text objects, like functions and arguments
+  {
+    "echasnovski/mini.ai",
+    config = function()
+      require("mini.ai").setup()
+    end,
+  },
+ 
+  -- jump around with "s"
+  {
+    "ggandor/leap.nvim",
+    config = function()
+      require("leap").add_default_mappings()
+    end,
+  },
+
+  -- better "f" and "t"
+  {
+    "ggandor/flit.nvim",
+    config = true,
+    dependencies = {
+      "tpope/vim-repeat"
+    }
+  },
+ 
+  -- smarter substitue, among other things
+  { "tpope/vim-abolish" },
+
+  -- todo comments
+  { "folke/todo-comments.nvim", config = true },
 }
