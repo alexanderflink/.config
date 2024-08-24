2d9f502f50d13481e70ce0529f49971aceed09e1
Alexander Flink <hi@alexanderflink.com>
2024-08-24 09:26:01 +0200

    update config

---
+++ fish/envs/klattermusen-qa.fish
@@ -0,0 +1,2 @@
+set -gx CENTRA_CHECKOUT_API_URL (op read op://bhoiw4aaby24lmfr6qg4wch3hq/.env.stage/CENTRA_CHECKOUT_API)
+set -gx CENTRA_CHECKOUT_API_KEY (op read op://bhoiw4aaby24lmfr6qg4wch3hq/.env.stage/CENTRA_CHECKOUT_API_KEY)
